85fd8f74efd2cc374fbaea49e9dc8363f07ee880
Jurgen Muca <jur1muc4@gmail.com>
2024-10-30 23:26:33 +0100

    Added js

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function customFunction(){
+    alert = 'navbar';
+}
+
+function changeTextOfP(){
+    document.getElementById('homePageP').innerHTML = 'This is a new '
+}
+function logAirlineDetails(event){
+    const airlineName = document.getElementById('airline-input');
+    alert='airlineName';
+}
